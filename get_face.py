17c901058140eaeed0462765f8dfe7ee01e5eef3
Uahh <hnyrarara@gmail.com>
2021-12-05 17:07:19 +0800

    Add files via upload

---
+++ get_face.py
@@ -0,0 +1,47 @@
+import os
+import cv2
+import time
+
+
+def getAllPath(dirpath, *args):
+    PathArray = []
+    for r, ds, fs in os.walk(dirpath):
+        for fn in fs:
+            if os.path.splitext(fn)[1] in args:
+                fname = os.path.join(r, fn)
+                PathArray.append(fname)
+    return PathArray
+
+
+def cut_face(sourcePath, targetPath, *args):
+    ImagePaths = getAllPath(sourcePath, *args)
+
+    count = 1
+    # haarcascade_frontalface_alt.xml为库训练好的分类器文件，下载opencv，安装目录中可找到
+    face_cascade = cv2.CascadeClassifier('C:\\Python310\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_alt.xml')
+    for imagePath in ImagePaths:
+        img = cv2.imread(imagePath)
+
+        if type(img) != str:
+            faces = face_cascade.detectMultiScale(img, 1.1, 3)
+            if len(faces):
+                for (x, y, w, h) in faces:
+                    # 设置人脸宽度大于16像素，去除较小的人脸
+                    if w >= 16 and h >= 16:
+                        # 以时间戳和读取的排序作为文件名称
+                        listStr = [str(count)]
+                        fileName = ''.join(listStr)
+                        # 扩大图片，可根据坐标调整
+                        X = int(x)
+                        W = min(int(x + w), img.shape[1])
+                        Y = int(y)
+                        H = min(int(y + h), img.shape[0])
+
+                        f = cv2.resize(img[Y:H, X:W], (W - X, H - Y))
+                        cv2.imwrite(targetPath + os.sep + '%s.jpg' % fileName, f)
+                        count += 1
+
+if __name__ == '__main__':
+    sourcePath = r'D:/Project/DDIngredientAppraisalDevice/picture/inami'
+    targetPath1 = r'D:/Project/DDIngredientAppraisalDevice/picture/inami2'
+    cut_face(sourcePath, targetPath1, '.jpg', '.JPG', 'png', 'PNG')
---
+++ get_picture.py
@@ -0,0 +1,46 @@
+import os
+import re
+import requests
+from get_face import cut_face
+
+def mkdir(path):
+    folder = os.path.exists(path)
+    if not folder:                   
+        os.makedirs(path)            
+
+
+
+idol_list = [
+    "nittaemi85", "aya_uchida", "mimori_suzuko", "pile_eric", "INFO_shikaco", "rippialoha", "tokui_sorangley", "nanjolno", "kusudaaina",
+    "anju_inami", "Rikako_Aida", "Saito_Shuka", "Aikyan_", "Kanako_tktk", "furihata_ai", "aina_suzuki723", "suwananaka", "box_komiyaarisa",
+    "box_komiyaarisa", "satohina1223", 
+    "aguri_onishi", "natyaaaaaaan07", "tomori_kusunoki", "MayuSgr", "kaor1n_n", "t_chiemi1006", "iRis_k_miyu", "kitoakari_1016", "sashide_m", "yano_hinaki35", "k_moeka_"
+    "SayuriDate", "Liyu0109", "MisakiNako_", "_Naomi_Payton_", "AoyamaNagisa"
+]
+
+for idol in idol_list:
+    jpgurl_list = []
+    for page in range(1, 100):
+        r = requests.get("https://twi.lovelive.cx/?id=" + idol +"&type=original&page=" + str(page))
+        pattern = re.compile('https://proxy.lovelive.cx/media.+?.jpg')
+        jpgurl = pattern.findall(repr(r.text))
+        for i in jpgurl:
+            jpgurl_list.append(i)
+
+    jpgurl_list = list(set(jpgurl_list))
+
+    cnt = 0
+    for jpgurl in jpgurl_list:
+        res = requests.get(jpgurl)
+        path_list = [os.getcwd(), '\\picture\\' + idol]
+        path = os.path.join(''.join(path_list))
+        mkdir(path)
+        path_list.append("\\")
+        path_list.append(str(cnt) + '.jpg')
+        file_path = os.path.join(''.join(path_list))
+        with open(file_path, 'wb') as jpg_file:
+            jpg_file.write(res.content)
+        cnt += 1
+
+    mkdir(path + "_cut")
+    cut_face(path, path + "_cut", '.jpg', '.JPG', 'png', 'PNG')
